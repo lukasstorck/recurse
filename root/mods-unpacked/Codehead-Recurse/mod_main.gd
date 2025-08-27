8f2d09fe35e9589c661c744f198f0b646bf6a394
Lukas <38840142+lukasstorck@users.noreply.github.com>
2025-08-27 13:43:06 +0200

    feat: add mod loading messages

--- root/mods-unpacked/Codehead-Recurse/mod_main.gd
+++ root/mods-unpacked/Codehead-Recurse/mod_main.gd
@@ -1,6 +1,11 @@
 extends Node
 
+const MOD_ID = "Codehead-Recurse"
 
 func _init():
+	ModLoaderLog.info("Init", MOD_ID)
 	ModLoaderMod.install_script_extension("res://mods-unpacked/Codehead-Recurse/extensions/dlcs/dlc_1/dlc_1_data.gd")
 	ModLoaderMod.install_script_extension("res://mods-unpacked/Codehead-Recurse/extensions/ui/menus/shop/base_shop.gd")
+
+func _ready():
+	ModLoaderLog.info("Ready", MOD_ID)
